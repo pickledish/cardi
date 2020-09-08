013e4e0acae2be7aff2068b10fc754fd7ed04a6d
Brandon Willett <bcwillett1@gmail.com>
2020-09-07 21:11:39 -0400

    Remove outdated messageview

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,11 @@
   import Tailwind from "./Tailwind.svelte";
 	import Router from 'svelte-spa-router'
 	import Overview from './views/Overview.svelte'
-	import MessageView from './views/MessageView.svelte'
 	import LogIn from './views/LogIn.svelte'
 
 	const routes = {
       //'/': About,
       '/app': Overview,
-	    '/message/:message_id': MessageView,
 	    '/login': LogIn,
 	    // This is optional, but if present it must be the last
 	    '*': Overview,
