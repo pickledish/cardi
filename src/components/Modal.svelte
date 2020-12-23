826256e6fb806e44d7645cd3c6b5850079252f8d
Brandon Willett <bcwillett1@gmail.com>
2020-12-23 14:20:40 -0500

    fix modal z-index in light mode

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -18,7 +18,7 @@
 </style>
 
 {#if show}
-<div class="blurred fixed top-0 left-0 w-screen h-screen bg-grey z-10">
+<div class="blurred fixed top-0 left-0 w-screen h-screen bg-grey z-30">
   <div class="h-full w-full bg-black bg-opacity-50 flex items-center justify-center" on:click|self={close}>
     <div class="w-5/6 md:w-1/2 bg-white dark:bg-desk-700 bg-opacity-100 p-8 rounded">
       <slot></slot>
