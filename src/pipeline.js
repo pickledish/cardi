ce8473b3b0523403e074c2065831dfb25c1309fa
Brandon Willett <bcwillett1@gmail.com>
2021-02-15 09:36:54 -0500

    support twitter images as well as og

--- src/pipeline.js
+++ src/pipeline.js
@@ -16,24 +16,24 @@ export async function getMetadata(url) {
 
   let fullText = await fetchPageText(url);
 
-  let titleRegex = new RegExp("<title[^>]*>([^<]+)<\/title>");
-  let ogRegex1 = new RegExp("<meta[^>]*og:title[^>]*content=\"([^\"]+)\"[^>]*\/?>");
-  let ogRegex2 = new RegExp("<meta[^>]*content=\"([^\"]+)\"[^>]*og:title[^>]*\/?>");
-  let imageRegex1 = new RegExp("<meta[^>]*og:image[^>]*content=\"([^\"]+)\"[^>]*\/?>");
-  let imageRegex2 = new RegExp("<meta[^>]*content=\"([^\"]+)\"[^>]*og:image[^>]*\/?>");
+  let titleRegex1 = new RegExp("<title[^>]*>([^<]+)<\/title>");
+  let titleRegex2 = new RegExp("<meta[^>]*(?:og|twitter):title[^>]*content=\"([^\"]+)\"[^>]*\/?>");
+  let titleRegex3 = new RegExp("<meta[^>]*content=\"([^\"]+)\"[^>]*(?:og|twitter):title[^>]*\/?>");
+  let imageRegex1 = new RegExp("<meta[^>]*(?:og|twitter):image[^>]*content=\"([^\"]+)\"[^>]*\/?>");
+  let imageRegex2 = new RegExp("<meta[^>]*content=\"([^\"]+)\"[^>]*(?:og|twitter):image[^>]*\/?>");
 
-  let titleMatch = fullText.match(titleRegex);
-  let ogMatch1 = fullText.match(ogRegex1);
-  let ogMatch2 = fullText.match(ogRegex2);
+  let titleMatch1 = fullText.match(titleRegex1);
+  let titleMatch2 = fullText.match(titleRegex2);
+  let titleMatch3 = fullText.match(titleRegex3);
   let imageMatch1 = fullText.match(imageRegex1);
   let imageMatch2 = fullText.match(imageRegex2);
 
-  if (titleMatch) {
-    response["title"] = titleMatch[1];
-  } else if (ogMatch1) {
-    response["title"] = ogMatch1[1];
-  } else if (ogMatch2) {
-    response["title"] = ogMatch2[1];
+  if (titleMatch1) {
+    response["title"] = titleMatch1[1];
+  } else if (titleMatch2) {
+    response["title"] = titleMatch2[1];
+  } else if (titleMatch3) {
+    response["title"] = titleMatch3[1];
   }
 
   if (imageMatch1) {
