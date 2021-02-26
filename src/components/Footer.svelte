b64c7feeb1758773f5234103137756256068fc7f
Brandon Willett <bcwillett1@gmail.com>
2021-02-26 10:46:34 -0500

    add links to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <div class="flex justify-center items-center w-full h-16 my-8">
   <span class="text-sm text-light">
-    Version COMMIT_REF_PLACEHOLDER, built on BUILD_DATE_PLACEHOLDER from COMMIT_SHA_PLACEHOLDER
+    Version <a href="https://github.com/pickledish/cardi/releases/tag/COMMIT_REF_PLACEHOLDER">COMMIT_REF_PLACEHOLDER</a>, built on BUILD_DATE_PLACEHOLDER from <a href="https://github.com/pickledish/cardi/tree/COMMIT_SHA_PLACEHOLDER">COMMIT_SHA_PLACEHOLDER</a>
   </span>
 </div>
